b8095cd7ae4f1e06bf2e326707f45e8f575740c9
vaibhavkanke456sh <shivanshmultiservices4508@gmail.com>
2025-09-27 21:44:08 +0530

    Ignore vite cache files

--- frontend/.vite-cache-1758875034884/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-CRSB3YAX.js";
-import "./chunk-URVJV3K3.js";
-import "./chunk-EQCVQC35.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
--- frontend/.vite-cache-1758964013291/deps_temp_8f205c65/react.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "./chunk-URVJV3K3.js";
-import "./chunk-EQCVQC35.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
